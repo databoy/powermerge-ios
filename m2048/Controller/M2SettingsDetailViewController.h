61924784400160cf503cebc6a24820ac09e62b8c
Chris Pitchford <chris@databoy.biz>
2016-01-11 15:21:02 -0700

    Include modification date

--- m2048/Controller/M2SettingsDetailViewController.h
+++ m2048/Controller/M2SettingsDetailViewController.h
@@ -3,6 +3,7 @@
 //  m2048
 //
 //  Created by Danqing on 3/24/14.
+//  Modified by Chris Pitchford on 1/11/2016
 //  Copyright © 2015 Chris Pitchford Publishing, LTD. All rights reserved.
 //
 
--- m2048/Controller/M2ViewController.h
+++ m2048/Controller/M2ViewController.h
@@ -2,7 +2,7 @@
 //  M2ViewController.h
 //  m2048
 //
-
+//  Modified by Chris Pitchford on 1/11/2016
 //  Copyright © 2015 Chris Pitchford Publishing, LTD. All rights reserved.
 //
 
